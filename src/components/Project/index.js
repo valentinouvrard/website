5550f4707200a6382880427e38687187038ce04f
Valentin Ouvrard <valentin.ouvrard@yahoo.fr>
2019-07-31 17:31:46 +0200

    fix(merge): Fix wrong merge

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -17,28 +17,12 @@ const ProjectImg = styled.img`
 
 const Title = styled.h2`
     margin-top: 10px;
-    font-weight: 150;
     font-size : 48px;
-<<<<<<< HEAD
-=======
-    @media (max-width: 960px) {
-        font-size: 36px;
-        line-height : 42px;
-    }
->>>>>>> 90b87c6c8b0ecb3742bc8e86a42d16d350b165a2
 `
 
 const Description = styled.p`
-  font-weight: 150;
   font-size : 24px;
   line-height : 32px;
-<<<<<<< HEAD
-=======
-  @media (max-width: 960px) {
-      font-size: 18px;
-      line-height : 26px;
-  }
->>>>>>> 90b87c6c8b0ecb3742bc8e86a42d16d350b165a2
 `
 const Techs = styled.div`
   margin-top: auto;
@@ -47,12 +31,7 @@ const Techs = styled.div`
 `
 
 const TechImg = styled.img`
-<<<<<<< HEAD
   height: 5em;
-=======
-  max-height: 5em;
-  max-width: 10em;
->>>>>>> 90b87c6c8b0ecb3742bc8e86a42d16d350b165a2
   margin: 10px;
 `
 
@@ -65,11 +44,7 @@ function Project(props) {
     return (
     <Container>
       <Grid container spacing={3}>
-<<<<<<< HEAD
         <Grid item md={12} lg={4}>
-=======
-        <Grid item sm={12} md={4}>
->>>>>>> 90b87c6c8b0ecb3742bc8e86a42d16d350b165a2
           <LineSeparator/>
           <Title>{props.project.title}</Title>
           <Description>{props.project.description}</Description>
@@ -80,11 +55,7 @@ function Project(props) {
             </Grid>
           </Techs>
         </Grid>
-<<<<<<< HEAD
         <Grid item md={12} lg={8}>
-=======
-        <Grid item sm={12} md={8}>
->>>>>>> 90b87c6c8b0ecb3742bc8e86a42d16d350b165a2
           <ProjectImg src={props.project.img}/>
         </Grid>
       </Grid>
